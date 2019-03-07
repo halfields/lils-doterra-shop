bee7bfa5fb0aa8f2980981207214dbb080bddff0
halfields <hfields697@aol.com>
2019-03-06 23:51:35 -0600

    added get '/signup' route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signup', to: 'users#new'
+  resources :users
+  resources :products
+  resources :orders
+  resources :line_items
+
+  resources :carts do 
+  	resources :line_items, only: [:index, :show] 
+  end
+
+
+
+  scope :admin do 
+  	resources :products, only: [:new, :create]
+  end
 end
